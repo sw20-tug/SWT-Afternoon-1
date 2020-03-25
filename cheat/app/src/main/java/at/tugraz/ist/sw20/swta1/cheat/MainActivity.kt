951884147ed78bff9212b06a57d85e73ff37a651
Paul Weissensteiner <paul.weissensteiner@student.tugraz.at>
2020-03-25 14:58:33 +0100

    CHEAT-001A Implemented activation of bluetooth and readout of paired devices [WP, GD]

--- cheat/app/src/main/java/at/tugraz/ist/sw20/swta1/cheat/MainActivity.kt
+++ cheat/app/src/main/java/at/tugraz/ist/sw20/swta1/cheat/MainActivity.kt
@@ -1,51 +1,18 @@
 package at.tugraz.ist.sw20.swta1.cheat
 
-import android.app.Activity
-import android.bluetooth.BluetoothAdapter
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import at.tugraz.ist.sw20.swta1.cheat.ui.main.MainFragment
-import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
-  
-  private val REQUEST_ENABLE_BLUETOOTH: Int = 1
-  private var bluetoothAdapter: BluetoothAdapter? = null
-  
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.main_activity)
-    if (savedInstanceState == null) {
-      supportFragmentManager.beginTransaction()
-        .replace(R.id.container, MainFragment.newInstance())
-        .commitNow()
-    }
-  
-    bluetoothAdapter= BluetoothAdapter.getDefaultAdapter()
-    if (bluetoothAdapter == null) {
-      // Device doesn't support bluetooth
-      Toast.makeText(this, "No Bluetooth available", Toast.LENGTH_SHORT).show()
-    } else {
-      if (!bluetoothAdapter!!.isEnabled) {
-        val enableBluetoothIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-        startActivityForResult(enableBluetoothIntent, REQUEST_ENABLE_BLUETOOTH)
-      }
-    }
-  }
-  
-  override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-    super.onActivityResult(requestCode, resultCode, data)
-    if(requestCode == REQUEST_ENABLE_BLUETOOTH) {
-      if (resultCode == Activity.RESULT_OK) {
-        if(bluetoothAdapter!!.isEnabled) {
-          Toast.makeText(this, "Bluetooth enabled", Toast.LENGTH_SHORT).show()
-        } else {
-          Toast.makeText(this, "Bluetooth disabled", Toast.LENGTH_SHORT).show()
+    
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
         }
-      } else if (resultCode == Activity.RESULT_CANCELED) {
-        Toast.makeText(this, "Bluetooth enabling cancelled", Toast.LENGTH_SHORT).show()
-      }
     }
-  }
 }
---
+++ cheat/app/src/main/java/at/tugraz/ist/sw20/swta1/cheat/bluetooth/BluetoothService.kt
@@ -0,0 +1,53 @@
+package at.tugraz.ist.sw20.swta1.cheat.bluetooth
+
+import android.app.Activity
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
+import android.util.Log
+
+class BluetoothService(private val adapter: BluetoothAdapter) {
+    
+    private var receiver : BroadcastReceiver? = null
+    
+    fun getPairedDevices() : List<BluetoothDevice> {
+        return adapter.bondedDevices.toList()
+    }
+    
+    fun discoverDevices(activity: Activity, onDeviceFound: (BluetoothDevice) -> Unit) {
+        if(receiver != null) {
+            activity.unregisterReceiver(receiver)
+        }
+        
+        // Create a BroadcastReceiver for ACTION_FOUND.
+        receiver = object : BroadcastReceiver() {
+            override fun onReceive(context: Context, intent: Intent) {
+                val action: String? = intent.action
+                when(action) {
+                    BluetoothDevice.ACTION_FOUND -> {
+                        // Discovery has found a device. Get the BluetoothDevice
+                        // object and its info from the Intent.
+                        val device: BluetoothDevice = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE)
+                        Log.println(Log.DEBUG, "Found device", device.name)
+                        onDeviceFound(device)
+                    }
+                }
+            }
+        }
+        
+        activity.registerReceiver(receiver, IntentFilter(BluetoothDevice.ACTION_FOUND))
+        adapter.startDiscovery()
+        Log.println(Log.DEBUG, "Bluetooth", "Start discovery")
+    }
+    
+    fun stopDiscovery(activity: Activity) {
+        adapter.cancelDiscovery()
+        Log.println(Log.DEBUG, "Bluetooth", "Stop discovery")
+        if(receiver != null) {
+            activity.unregisterReceiver(receiver)
+        }
+    }
+}
