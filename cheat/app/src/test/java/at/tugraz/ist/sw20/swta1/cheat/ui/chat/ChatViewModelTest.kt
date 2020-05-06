724ac7f8340763a26d16a4dff630b54b6fdf82da
Mattis Simon Turin-Zelenko <turin-zelenko@student.tugraz.at>
2020-05-06 15:06:10 +0200

    CHEAT-012 Delete message test [TM, SL]

--- cheat/app/src/test/java/at/tugraz/ist/sw20/swta1/cheat/ui/chat/ChatViewModelTest.kt
+++ cheat/app/src/test/java/at/tugraz/ist/sw20/swta1/cheat/ui/chat/ChatViewModelTest.kt
@@ -1,6 +1,7 @@
 package at.tugraz.ist.sw20.swta1.cheat.ui.chat
 
 import junit.framework.Assert.assertEquals
+import junit.framework.Assert.assertTrue
 import org.junit.Test
 import java.util.*
 
@@ -34,5 +35,21 @@ internal class ChatViewModelTest {
         assertEquals(-1, scrollPosition2)
         assertEquals(entry2, viewModel.getChatEntries()[0])
         assertEquals(1, viewModel.getChatEntries().size)
+    }
+
+    @Test
+    fun testDeleteChatEntry() {
+        val entry = ChatEntry("Hi, this is a test msg",false, false, Date())
+        val deleteEntry = entry.clone() as ChatEntry
+        deleteEntry.setDeleted()
+
+        val viewModel = ChatViewModel()
+
+        val scrollPosition1 = viewModel.insertMessage(entry)
+        val scrollPosition2 = viewModel.insertMessage(deleteEntry)
+
+        assertEquals(0, scrollPosition1)
+        assertEquals(-1, scrollPosition2)
+        assertTrue(viewModel.getChatEntries()[0].isDeleted())
     }
 }
